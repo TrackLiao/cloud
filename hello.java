dac51524d1274b8d82389c1259e8d4fd12ee7a17
TrackLiao <track.liao@gmail.com>
2017-04-09 22:06:54 +0800

    add hello.java

---
+++ hello.java
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: hello.java
+	> Author: TrackLiao 
+	> Mail: track.liao@gmail.com 
+	> Created Time: Sun 09 Apr 2017 10:05:56 PM HKT
+ ************************************************************************/
+
+public class hello{
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
+
